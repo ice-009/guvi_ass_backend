06ac4a37038ff986ca94722a80f93bf2a5e502fa
Armaan Shukla <21bma009@nith.ac.in>
2023-10-14 23:09:27 +0530

    dfdfdfdfdsfdfdfdfdf

--- helpers/validationSchema.js
+++ helpers/validationSchema.js
@@ -1,7 +1,7 @@
 const Joi = require("@hapi/joi")
 const authSchema= Joi.object({
     email : Joi.string().email().lowercase().required(),
-    username: Joi.string().min(2).required(),
+    username: Joi.string().min(2),
     password: Joi.string().min(2).required()
 
 })
