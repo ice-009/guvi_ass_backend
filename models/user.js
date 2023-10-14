3c5b28a6b40fd8c788b91d5e22830e9ccd03fe14
Armaan Shukla <21bma009@nith.ac.in>
2023-10-14 22:29:20 +0530

    dfdfdf

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true, lowercase: true },
-  username: {type: String, required: true, unique: true, lowercase},
+  username: {type: String, required: true, unique: true},
   password: { type: String, required: true },
   revokedRefreshTokens: [{ type: String }],
 });
